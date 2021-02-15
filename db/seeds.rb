18c8bcbde98300bb197e3caa6edf28ebd463660a
Ubuntu <ubuntu@ip-172-31-23-152.us-east-2.compute.internal>
2021-02-15 23:02:39 +0000

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,46 +1,46 @@
 User.create!([
-  {email: "test@test.com", password: "testtest", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, admin: nil, computer_id: nil},
-  {email: "admin@admin.com", password: "admin123", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, admin: true, computer_id: nil}
+  {id: 2, created_at: "2021-02-14 21:14:51", updated_at: "2021-02-15 15:38:03", email: "test@test.com", encrypted_password: "$2a$12$9GdlbBxJWvxNVwYzFXl2o.5KkrcXQxhb/5uBKTGtQCiiBnEagt7fe", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, admin: nil, computer_id: nil},
+  {id: 3, created_at: "2021-02-14 22:22:22", updated_at: "2021-02-15 14:11:57", email: "admin@admin.com", encrypted_password: "$2a$12$m.w4xXR7d3viBlrFOKYSjOBW6IoPxO66.EUVKP6BtHuUyjoQRCzsa", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, admin: true, computer_id: nil}
 ])
 Gpu.create!([
-  {name: "qwfp", ram: 6},
-  {name: "nvidia 1660s", ram: 6}
+  {id: 1, name: "qwfp", ram: 6, created_at: "2021-02-14 18:58:13", updated_at: "2021-02-14 18:58:13"},
+  {id: 2, name: "nvidia 1660s", ram: 6, created_at: "2021-02-14 22:30:27", updated_at: "2021-02-14 22:30:27"}
 ])
 Comment.create!([
-  {user_id: 2, game_id: 2, content: "123213tarst"},
-  {user_id: 3, game_id: 2, content: "wqpqwfqwfp"},
-  {user_id: 3, game_id: 2, content: "qwpfpqwffwqp"},
-  {user_id: 2, game_id: 2, content: "aienrstniersaein"},
-  {user_id: 2, game_id: 2, content: "qwpffpqwqfwp"},
-  {user_id: 2, game_id: 2, content: "zxvczxcvxcv"},
-  {user_id: 2, game_id: 1, content: "zvcxcxvzxcvz"},
-  {user_id: 2, game_id: 1, content: "zxcvzxcv"}
+  {id: 4, user_id: 2, game_id: 2, content: "123213tarst", created_at: "2021-02-15 11:28:35", updated_at: "2021-02-15 11:28:35"},
+  {id: 5, user_id: 3, game_id: 2, content: "wqpqwfqwfp", created_at: "2021-02-15 11:45:03", updated_at: "2021-02-15 11:45:03"},
+  {id: 6, user_id: 3, game_id: 2, content: "qwpfpqwffwqp", created_at: "2021-02-15 13:01:50", updated_at: "2021-02-15 13:01:50"},
+  {id: 7, user_id: 2, game_id: 2, content: "aienrstniersaein", created_at: "2021-02-15 15:02:19", updated_at: "2021-02-15 15:02:19"},
+  {id: 8, user_id: 2, game_id: 2, content: "qwpffpqwqfwp", created_at: "2021-02-15 15:02:36", updated_at: "2021-02-15 15:02:36"},
+  {id: 9, user_id: 2, game_id: 2, content: "zxvczxcvxcv", created_at: "2021-02-15 15:11:35", updated_at: "2021-02-15 15:11:35"},
+  {id: 10, user_id: 2, game_id: 1, content: "zvcxcxvzxcvz", created_at: "2021-02-15 15:11:43", updated_at: "2021-02-15 15:11:43"},
+  {id: 11, user_id: 2, game_id: 1, content: "zxcvzxcv", created_at: "2021-02-15 15:13:15", updated_at: "2021-02-15 15:13:15"}
 ])
 Article.create!([
-  {user_id: 3, title: "qwfpqwf", content: "rsetoastr"},
-  {user_id: 2, title: "abcd", content: "qweifp"}
+  {id: 1, user_id: 3, title: "qwfpqwf", content: "rsetoastr", created_at: "2021-02-15 11:41:30", updated_at: "2021-02-15 11:41:30"},
+  {id: 2, user_id: 2, title: "abcd", content: "qweifp", created_at: "2021-02-15 22:05:03", updated_at: "2021-02-15 22:05:03"}
 ])
 Genre.create!([
-  {name: "RPG"},
-  {name: "RTS"}
+  {id: 1, name: "RPG", created_at: "2021-02-14 18:59:07", updated_at: "2021-02-14 18:59:07"},
+  {id: 2, name: "RTS", created_at: "2021-02-14 22:30:02", updated_at: "2021-02-14 22:30:02"}
 ])
 Cpu.create!([
-  {name: "abcd", cores: 8},
-  {name: "intel i5 8250u", cores: 4}
+  {id: 1, name: "abcd", cores: 8, created_at: "2021-02-14 18:58:00", updated_at: "2021-02-14 18:58:00"},
+  {id: 2, name: "intel i5 8250u", cores: 4, created_at: "2021-02-14 22:30:17", updated_at: "2021-02-14 22:30:17"}
 ])
 Computer.create!([
-  {user_id: 1, cpu_id: 1, gpu_id: 1, ram: 8},
-  {user_id: 2, cpu_id: 2, gpu_id: 2, ram: 24}
+  {id: 1, user_id: 1, cpu_id: 1, gpu_id: 1, ram: 8, created_at: "2021-02-14 18:58:22", updated_at: "2021-02-14 18:58:22"},
+  {id: 3, user_id: 2, cpu_id: 2, gpu_id: 2, ram: 24, created_at: "2021-02-15 12:18:20", updated_at: "2021-02-15 12:18:20"}
 ])
 Game.create!([
-  {name: "abcd", description: "aorsieroieas aroeisoaier", price: 20, studio_id: 1, genre_id: 1},
-  {name: "Doom", description: "abcd", price: 20, studio_id: 1, genre_id: 2},
-  {name: "qwfpqwfp", description: "arstrastrsta", price: 234, studio_id: 2, genre_id: 1}
+  {id: 1, name: "abcd", description: "aorsieroieas aroeisoaier", price: 20, studio_id: 1, genre_id: 1, created_at: "2021-02-14 18:59:12", updated_at: "2021-02-14 18:59:12"},
+  {id: 2, name: "Doom", description: "abcd", price: 20, studio_id: 1, genre_id: 2, created_at: "2021-02-14 22:39:37", updated_at: "2021-02-15 12:50:01"},
+  {id: 4, name: "qwfpqwfp", description: "arstrastrsta", price: 234, studio_id: 2, genre_id: 1, created_at: "2021-02-15 22:39:31", updated_at: "2021-02-15 22:39:31"}
 ])
 Order.create!([
-  {user_id: 3, game_id: 1}
+  {id: 4, user_id: 3, game_id: 1, created_at: "2021-02-15 22:12:39", updated_at: "2021-02-15 22:12:39"}
 ])
 Studio.create!([
-  {name: "test1"},
-  {name: "abcd"}
+  {id: 1, name: "test1", created_at: "2021-02-14 18:58:58", updated_at: "2021-02-14 18:58:58"},
+  {id: 2, name: "abcd", created_at: "2021-02-14 22:29:52", updated_at: "2021-02-14 22:29:52"}
 ])
